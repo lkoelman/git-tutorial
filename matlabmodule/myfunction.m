e955a9d15d897b4c0752ea685e62bfa2b0a05b50
Ben O'Callaghan <ben.o-callaghan@ucdconnect.ie>
2018-10-24 11:23:02 +0100

    Changed the function
    Now squaring the third term and multiplying by the su of the first two

--- matlabmodule/myfunction.m
+++ matlabmodule/myfunction.m
@@ -1,6 +1,6 @@
 function res = addmul(x,y,z)
-% Add first two arguments and multiply result by third argument
+% Add first two arguments and multiply result by the square of the third argument
 
-res = (x + y) * z
+res = (x + y) * z^2
 
 end
